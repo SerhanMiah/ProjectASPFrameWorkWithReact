3b7aed3488f6a09b5c8065ab36dcf7792b0fc3d8
Serhan miah <101991709+SerhanMiah@users.noreply.github.com>
2023-05-14 15:18:10 +0100

    first commit using VS template

---
+++ ASPFrameWorkWithReact/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace ASPFrameWorkWithReact.Models
+{
+	public class Product
+	{
+
+
+		// Creating the Model, code first approach 
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Category { get; set; }
+		public string CategoryName { get; set; }
+		public string CategoryDescription { get; set; }
+
+	}
+}
